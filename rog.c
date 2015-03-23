257393a6282ec8e2634f96116aef5fe3690b21b3
usipeus <arisaka1@gmail.com>
2015-03-23 14:30:16 -0500

    Add functions and direction definitions

--- rog.c
+++ rog.c
@@ -1,11 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int* entities;
 
-void draw_map(name);
+char* map;
+
+// map related functions
+void draw_map(char* m);
+char* load_map(char* name, int height, int width);
+void replace_with_entities(char* map, int* entities);
+
+// entity related functions
+int* add_entity(int* e);
+int move_entity(int id, int direction);
+
+void game_setup();
+void game_loop();
+
+// map related functions
+void draw_map(char* m) {
+	
+}
+
+char* load_map(char* name, int height, int width) {
+
+}
+
+void replace_with_entities(char* map, int* e) {
+	for (
+
+}
 
 int main(int argc, char* argv[])
 {
-
+	game_setup();
+	game_loop();
     return 0;
 }
---
+++ rog.h
@@ -0,0 +1,18 @@
+#ifndef MAP_WIDTH
+#define MAP_WIDTH 80
+#endif
+
+#ifndef MAP_HEIGHT
+#define MAP_HEIGHT 25
+#endif
+
+#define D_UP (-MAP_WIDTH)
+#define D_UL (-MAP_WIDTH - 1)
+#define D_UR (-MAP_WIDTH + 1)
+
+#define D_LE (-1)
+#define D_RI (1)
+
+#define D_DN (MAP_WIDTH)
+#define D_DL (MAP_WIDTH - 1)
+#define D_DR (MAP_WIDTH + 1)
