ba30e821178f91cdb0218e566f96d466a5bac851
Young Li <arisaka1@gmail.com>
2015-07-24 19:03:04 -0700

    fixed all valgrind errors

--- rog.c
+++ rog.c
@@ -1,32 +1,40 @@
 #include "rog.h"
 
-// game functions
 void game_setup();
 void game_loop();
 
-// main function
-int main(int argc, char * argv[]) {
+int
+main ()
+{
 	game_setup();
 	game_loop();
 
 	return 0;
 }
 
-void game_setup() {
+void
+game_setup()
+{
 	map m;
 	m.height = 4;
 	m.width = 4;
-	m.map = malloc((m.height * m.width + 1) * sizeof(char));
-	strcpy(m.map, "#####..##..#####");
-	m.characters = malloc(2 * sizeof(char));
-	strcpy(m.characters, "@a");
-	m.locations = malloc(2 * sizeof(int));
+	m.map = malloc ((m.height * m.width + 1) * sizeof(char));
+	strcpy (m.map, "#####..##..#####");
+	m.characters = malloc (3 * sizeof(char));
+	strcpy(m.characters, "a@");
+	m.locations = malloc (2 * sizeof(int));
 	m.locations[0] = 5;
-	m.locations[1] = 9;
+	m.locations[1] = 6;
 
-	draw_map(m);
+	draw_map (m);
+	
+	free (m.map);
+	free (m.characters);
+	free (m.locations);
 }
 
-void game_loop() {
+void
+game_loop ()
+{
 
 }
