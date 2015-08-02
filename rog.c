9c89f75a2fb16f606dc1c4d01dea4a689a1e9bf7
Young Li <arisaka1@gmail.com>
2015-08-01 23:19:14 -0700

    Test map drawing

--- rog.c
+++ rog.c
@@ -18,16 +18,18 @@ game_setup()
 	map m;
 	m.height = 4;
 	m.width = 4;
-	m.map = malloc ((m.height * m.width + 1) * sizeof(char));
+	m.map = malloc ((m.height * m.width + 1) * sizeof (char));
 	strcpy (m.map, "#####..##..#####");
-	m.characters = malloc (3 * sizeof(char));
-	strcpy(m.characters, "a@");
-	m.locations = malloc (2 * sizeof(int));
-	m.locations[0] = 5;
-	m.locations[1] = 6;
+	m.characters = malloc (5 * sizeof (char));
+	strcpy (m.characters, "@aDc");
+	m.locations = malloc(4 * sizeof (unsigned int));
+	(m.locations)[0] = 5;
+	(m.locations)[1] = 9;
+	(m.locations)[2] = 10;
+	(m.locations)[3] = 6;
 
-	draw_map (m);
-	
+	draw_map (&m);
+
 	free (m.map);
 	free (m.characters);
 	free (m.locations);
