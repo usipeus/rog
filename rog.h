21474cfe8a971451a086d18ee9964b7995b72b76
Young Li <arisaka1@gmail.com>
2015-08-01 23:19:53 -0700

    Use map * for draw_map() and fix wrong num of characters drawn

--- rog.h
+++ rog.h
@@ -12,47 +12,75 @@ typedef struct {
 
 /* map related functions */
 void
-draw_map (map m);
+draw_map (map * m);
 
 map *
 load_map ();
 
 /* character related functions */
 void
-add_character (map m,
+add_character (map * m,
 	       char tile,
-	       unsigned int location);
+	       unsigned int loc);
 
 void
-del_character ();
+del_character (map * m,
+	       unsigned int id);
 
 void
-move_character ();
+move_character (map * m,
+		unsigned int id,
+		int dir);
 
 /* function definitions */
 
 void
-draw_map (map m)
+draw_map (map * m)
 {
 	unsigned int i, j;
-	char to_print;
-	for (i = 0; i < strlen (m.map); i++)
+	char * map_with_chars = malloc (strlen (m->map) * sizeof (char) + 1);
+	strcpy (map_with_chars, m->map);
+
+	/* replace the tiles in m.locations[] with a char from m.characters[] */
+	printf("Num of chars to replace: %ld\n", strlen (m->characters));
+	for (i = 0; i < strlen (m->characters); i++)
 	{
-		for (j = 0; j < sizeof (m.locations) / sizeof (*(m.locations)); j++)
-		{
-			if (m.locations[j] == i)
-			{
-				to_print = m.characters[j];
-			}
-			else
-			{
-				to_print = m.map[i];
-			}
-		}
-
-		printf ("%c", to_print);
-
-		if ((i + 1) % m.width == 0)
-			printf ("\n");
+		map_with_chars[(m->locations)[i]] = (m->characters)[i];
+		printf("Replaced tile %i w/ char %c\n", i, (m->characters)[i]);
 	}
+
+	for (j = 0; j < strlen (map_with_chars); j++)
+	{
+		printf ("%c", map_with_chars[j]);
+
+		if ((j + 1) % m->width == 0)
+		{
+			printf ("\n");
+		}
+	}
+
+	free(map_with_chars);
+}
+
+void
+add_character (map * m,
+	       char tile,
+	       unsigned int loc)
+{
+
+}
+
+void
+del_character (map * m,
+	       unsigned int id)
+{
+
+}
+
+void
+move_character (map * m,
+		unsigned int id,
+		int dir)
+{
+	(m->locations)[id] += dir;
 }
